45dcf9f5bb883ad9ddfefa9d7ec87b8984dc2e75
sudha240 <sudhavelpuri24@gmail.com>
2023-10-13 11:52:28 +0530

    [+][Version Update v1.2.0][Change Icons, Minor style changes]

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -8,7 +8,7 @@ import RestartAltIcon from '@mui/icons-material/RestartAlt'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import MenuIcon from '@mui/icons-material/Menu'
 import Toggle from './Toggle'
-import { Divider, IconButton} from '@mui/material'
+import { Divider, IconButton, ListItemButton } from '@mui/material'
 
 const CustomDrawer = (props) => {
   const { resetBoard, setDark, isDark, defaultOpen } = props
@@ -21,7 +21,11 @@ const CustomDrawer = (props) => {
   }, [defaultOpen])
 
   const toggleDrawer = (open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return
     }
 
@@ -58,7 +62,10 @@ const CustomDrawer = (props) => {
     }[text])
 
   const list = () => (
-    <Box sx={{ width: drawPos === 'bottom' ? 'auto' : 250 }} role="presentation">
+    <Box
+      sx={{ width: drawPos === 'bottom' ? 'auto' : 250 }}
+      role='presentation'
+    >
       <List>
         {Object.keys(componentMapper).map((text, index) => (
           <ListItem key={index} onClick={toggleDrawer(false)}>
@@ -67,21 +74,43 @@ const CustomDrawer = (props) => {
           </ListItem>
         ))}
         <Divider />
-        <IconButton onClick={() => window.open('https://github.com/srikiran1707', '_blank')}>
+      </List>
+      <List style={{ position: 'absolute', bottom: 0, width: 250 }}>
+        <p
+          style={{
+            all: 'unset',
+            display: 'revert',
+            textAlign: 'center',
+            fontSize: '0.8rem',
+          }}
+        >
+          v1.2.0
+        </p>
+        <Divider />
+        <ListItemButton
+          onClick={() =>
+            window.open('https://github.com/srikiran1707', '_blank')
+          }
+        >
           <GitHubIcon />
           <span style={{ fontSize: '.75rem', paddingLeft: '5px' }}>
             Developed by <strong>Srikiran Velpuri</strong>
           </span>
-        </IconButton>
+        </ListItemButton>
       </List>
     </Box>
   )
   return (
     <div>
-      <IconButton aria-label="menu" onClick={toggleDrawer(true)}>
+      <IconButton aria-label='menu' onClick={toggleDrawer(true)}>
         <MenuIcon />
       </IconButton>
-      <SwipeableDrawer anchor={drawPos} open={state} onClose={toggleDrawer(false)} onOpen={toggleDrawer(true)}>
+      <SwipeableDrawer
+        anchor={drawPos}
+        open={state}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+      >
         {list()}
       </SwipeableDrawer>
     </div>
